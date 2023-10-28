92ca470acea8f2db4f8d606911476ed9653473fe
shazilhasan <146967418+shazilhasan@users.noreply.github.com>
2023-10-28 19:38:53 +0530

    mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/loginAuth',{
+mongoose.connect('mongodb://technologics-server:hd4a9UriDrebEAnhxNvWpLdDnUpiSFaiCXTJ4R6wXF54cHkIiCGbvsXZK666gHrsoBa3Tqrwjs3QACDbICwNHA==@technologics-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@technologics-server@',{
     useNewUrlParser:true,
     useFindAndModify:true,
     useUnifiedTopology:true,
